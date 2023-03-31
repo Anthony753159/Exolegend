5e75e6847a564a97a580e07cdd32762165770c6a
Selenn Lamson <perso@thomas-lamson.com>
2023-03-31 17:26:00 +0200

    Strategy states

--- src/strategy.cpp
+++ src/strategy.cpp
@@ -2,8 +2,13 @@
 
 #include "strategy.hpp"
 
+#define WHEEL_TURN_SPEED 0.2
+#define WHEEL_FORWARD_SPEED 0.6
+
 Strategy::Strategy(Gladiator *gladiator) : m_gladiator(gladiator)
 {
+  m_state_index = 0;
+  m_state = m_state_sequence[m_state_index];
 }
 
 Strategy::~Strategy()
@@ -12,11 +17,46 @@ Strategy::~Strategy()
 
 void Strategy::Update()
 {
-  m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.6);
-  m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.6);
-  delay(2000);
+  switch (m_state)
+  {
+  case IDLE:
+    m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0);
+    m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0);
+    break;
 
-  m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, -0.6);
-  m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, -0.6);
-  delay(2000);
+  case FORWARD:
+    m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, WHEEL_FORWARD_SPEED);
+    m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, WHEEL_FORWARD_SPEED);
+    break;
+
+  case BACKWARD:
+    m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, -WHEEL_FORWARD_SPEED);
+    m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, -WHEEL_FORWARD_SPEED);
+    break;
+
+  case LEFT:
+    m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, WHEEL_TURN_SPEED);
+    m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, -WHEEL_TURN_SPEED);
+    break;
+
+  case RIGHT:
+    m_gladiator->control->setWheelSpeed(WheelAxis::RIGHT, -WHEEL_TURN_SPEED);
+    m_gladiator->control->setWheelSpeed(WheelAxis::LEFT, WHEEL_TURN_SPEED);
+    break;
+  }
+
+  UpdateState();
+
+  delay(100);
+  m_updates++;
+}
+
+void Strategy::UpdateState()
+{
+  if (m_updates > 20)
+  {
+    m_updates = 0;
+    m_state_index = (m_state_index + 1) % 8;
+    m_state = m_state_sequence[m_state_index];
+  }
 }
--- src/strategy.hpp
+++ src/strategy.hpp
@@ -9,7 +9,31 @@ public:
   ~Strategy();
 
   void Update();
+  void UpdateState();
 
 private:
   Gladiator *m_gladiator;
+
+  enum State
+  {
+    IDLE,
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+  } m_state;
+
+  size_t m_state_index;
+  State m_state_sequence[8] = {
+      State::IDLE,
+      State::FORWARD,
+      State::LEFT,
+      State::FORWARD,
+      State::RIGHT,
+      State::FORWARD,
+      State::BACKWARD,
+      State::IDLE,
+  };
+
+  size_t m_updates = 0;
 };
